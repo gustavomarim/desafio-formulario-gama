87df664640a58f35764d344bc9d7b0f2c9fbcf15
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-08-21 19:03:49 -0300

    Add. função de renderização de campos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [formValues, setFormValues] = useState({})
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const formData = new FormData(e.target)
+    const data = Object.fromEntries(formData) // Obj com todos os dados do form
+
+    console.log('handleSubmit', data);
+  }
+
+  console.log('formValues', formValues);
+
   return (
-    <div id="main-container" >
+    <div id="main-container" onSubmit={handleSubmit} >
       <h1>Cadastro de Currículo</h1>
 
       <form id="register-form">
+
         <div className="half-box" >
           <label htmlFor="name">Nome</label>
-          <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo" />
+          <input type="text" name="nome" id="nome" placeholder="Digite seu nome completo"
+            onChange={handleInputChange} value={formValues.nome || ''} />
         </div>
 
         <div className="half-box" >
           <label htmlFor="cargo" >Cargo Pretendido</label>
-          <input type="text" name="cargo" id="cargo" />
+          <input type="text" name="cargo" id="cargo" onChange={handleInputChange}
+            value={formValues.cargo || ''} />
         </div>
 
         {/*   <div className="full-box">
@@ -22,22 +44,25 @@ function App() {
         </div> */}
 
         <div className="data-nascimento">
-          <label htmlFor="dataMes" id="dataMes">Data de Nascimento</label>
-          <select dataMes id="dataMes">
+          <label >Data de Nascimento</label>
+          <select name="mesNascimento" onChange={handleInputChange}
+            value={formValues.mesNascimento || ''}>
             <option value="empty">Mês</option>
             <option value="janeiro">janeiro</option>
             <option value="fevereiro">fevereiro</option>
             <option value="marco">março</option>
           </select>
 
-          <select dataMes id="dataDia">
+          <select name="diaNascimento" onChange={handleInputChange}
+            value={formValues.diaNascimento || ''}>
             <option value="empty">Dia</option>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
           </select>
 
-          <select dataMes id="dataAno">
+          <select name="anoNascimento" onChange={handleInputChange}
+            value={formValues.anoNascimento || ''}>
             <option value="empty" >Ano</option>
             <option value="1970">1970</option>
             <option value="1970">1970</option>
@@ -46,8 +71,9 @@ function App() {
         </div>
 
         <div className="estado-civil">
-          <label htmlFor="estado-civil" id="estado-civil">Estado Civíl</label>
-          <select estado-civil id="estado-civil">
+          <label id="estado-civil">Estado Civíl</label>
+          <select name="estadoCivil" onChange={handleInputChange}
+            value={formValues.estadoCivil || ''} >
             <option value="empty"></option>
             <option value="solteiro">Solteiro</option>
             <option value="casado">Casado</option>
@@ -56,8 +82,9 @@ function App() {
         </div>
 
         <div className="sexo">
-          <label htmlFor="sexo" id="sexo">Sexo</label>
-          <select sexo id="sexo">
+          <label >Sexo</label>
+          <select name="sexo" onChange={handleInputChange}
+            value={formValues.sexo || ''} >
             <option value="empty"></option>
             <option value="masculino">Masculino</option>
             <option value="feminino">Feminino</option>
@@ -65,65 +92,67 @@ function App() {
         </div>
 
         <div className="full-box" >
-          <label htmlFor="endereco">Endereço</label>
-          <input type="text" name="endereco" id="endereco" placeholder="ex: Nome da Rua, nº. Bloco nº, AP nº" />
+          <label>Endereço</label>
+          <input type="text" name="endereco" id="endereco" placeholder="ex: Nome da Rua, nº. Bloco nº, AP nº"
+            onChange={handleInputChange} value={formValues.endereco || ''} />
         </div>
 
         <div className="half-box" >
-          <label htmlFor="bairro">Bairro</label>
-          <input type="text" name="bairro" id="bairro" placeholder="" />
+          <label >Bairro</label>
+          <input type="text" name="bairro" id="bairro" onChange={handleInputChange} value={formValues.bairro || ''} />
         </div>
 
         <div className="half-box" >
-          <label htmlFor="cidade">Cidade</label>
-          <input type="text" name="cidade" id="cidade" placeholder="" />
+          <label >Cidade</label>
+          <input type="text" name="cidade" id="cidade" onChange={handleInputChange} value={formValues.cidade || ''} />
         </div>
 
         <div className="triple-box">
-          <label htmlFor="cep">CEP</label>
-          <input type="text" name="cep" id="cep" placeholder="" />
+          <label >CEP</label>
+          <input type="text" name="cep" id="cep" onChange={handleInputChange} value={formValues.cep || ''} />
         </div>
 
         <div className="triple-box">
-          <label htmlFor="telefone1">Telefone Fixo 1</label>
-          <input type="text" name="telefone2" id="telefone2" placeholder="" />
+          <label >Telefone Fixo 1</label>
+          <input type="text" name="telefone1" id="telefone1" onChange={handleInputChange} value={formValues.telefone1 || ''} />
         </div>
 
         <div className="triple-box">
-          <label htmlFor="telefone2">Telefone Fixo 2</label>
-          <input type="text" name="telefone2" id="telefone2" placeholder="" />
+          <label >Telefone Fixo 2</label>
+          <input type="text" name="telefone2" id="telefone2" onChange={handleInputChange} value={formValues.telefone2 || ''} />
         </div>
 
         <div className="triple-box">
-          <label htmlFor="celular">Celular</label>
-          <input type="text" name="celular" id="celular" placeholder="" />
+          <label >Celular</label>
+          <input type="text" name="celular" id="celular" onChange={handleInputChange} value={formValues.celular || ''} />
         </div>
 
         <div className="triple-box">
-          <label htmlFor="contato">Contato</label>
-          <input type="text" name="contato" id="contato" placeholder="" />
+          <label >Contato</label>
+          <input type="text" name="contato" id="contato" onChange={handleInputChange} value={formValues.contato || ''} />
         </div>
 
         <div className="triple-box">
           <label htmlFor="email">E-mail</label>
-          <input type="email" name="email" id="email" placeholder="ex: example@myname.com" />
+          <input type="email" name="email" id="email" placeholder="ex: example@myname.com" onChange={handleInputChange} value={formValues.email || ''} />
         </div>
 
         {/* <h1 id="documentos">Documentos</h1> */}
 
         <div className="quarter-box">
           <label htmlFor="identidade">Identidade</label>
-          <input type="text" name="identidade" id="identidade" placeholder="" />
+          <input type="text" name="identidade" id="identidade" onChange={handleInputChange} value={formValues.identidade || ''} />
         </div>
 
         <div className="quarter-box">
-          <label htmlFor="cpf">CPF</label>
-          <input type="text" name="cpf" id="cpf" placeholder="" />
+          <label >CPF</label>
+          <input type="text" name="cpf" id="cpf" onChange={handleInputChange} value={formValues.cpf || ''} />
         </div>
 
         <div className="quarter-box" >
-          <label htmlFor="veiculo" id="veiculo">Possui Veículo</label>
-          <select veiculo id="veiculo">
+          <label id="veiculo">Possui Veículo</label>
+          <select name="veiculo" onChange={handleInputChange}
+            value={formValues.veiculo || ''}>
             <option value="empty"></option>
             <option value="veiculo-sim">Sim</option>
             <option value="veiculo-nao">Não</option>
@@ -131,11 +160,15 @@ function App() {
         </div>
 
         <div className="quarter-box" >
-          <label htmlFor="cnh" id="veiculo">Habilitação</label>
-          <select veiculo id="cnh">
-            <option value="empty"></option>
-            <option value="cnh-sim">Sim</option>
-            <option value="cnh-nao">Não</option>
+          <label id="veiculo">Habilitação</label>
+          <select name="categoriaCnh" onChange={handleInputChange}
+            value={formValues.categoriaCnh || ''}>
+            <option value="categoria">Categoria</option>
+            <option value="categoria-a">A</option>
+            <option value="categoria-b">B</option>
+            <option value="categoria-c">C</option>
+            <option value="categoria-d">D</option>
+            <option value="categoria-e">E</option>
           </select>
         </div>
 
