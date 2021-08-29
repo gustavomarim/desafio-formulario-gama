2af2f981ca8b777d277a6339a55f02d0b3e68ba4
Gustavo Dantas Marim <gustavo.dantas.marim@outlook.com>
2021-08-29 18:28:30 -0300

    Configura porta do Heroku

--- src/server/MongoDBServer.js
+++ src/server/MongoDBServer.js
@@ -16,4 +16,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 app.use(routes);
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3001);
